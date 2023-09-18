19577d8271538634a8a04ec162610341d34dec2d
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-18 14:22:23 +0200

    Add project files.

---
+++ 22. Setters and Getters/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Setters_and_Getters
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+
+        //Constructor
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            rating = aRating;
+        }
+        public string Rating
+        {
+            get { 
+                return rating; }
+            set { 
+                if value =="S" || value=="PG"|| value=="PG-13" || value == "R"{
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+                    
+                }
+        }
+    }
+}
---
+++ 22. Setters and Getters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Setters_and_Getters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1= new Movie("Titanic","Jim","PG-13");
+            Movie movie2 = new Movie("27 Dresses", "Nono", "Cat");
+
+            //Freeze Console
+            Console.ReadLine();
+        }
+    }
+}
